5e97284492875ade59b9edd9965db068703b595c
Awaddd <34748292+Awaddd@users.noreply.github.com>
2025-06-21 19:12:26 +0100

    Reduce visual noise from info card

--- components/InfoCard.tsx
+++ components/InfoCard.tsx
@@ -11,12 +11,12 @@ export default function ({ }: Props) {
     const isMobile = useIsMobile()
 
     return (
-        <div className="w-full min-h-full pt-6 pb-4 px-6 bg-primary border shadow-xs rounded-lg">
+        <div className="w-full min-h-full pt-6 pb-4 px-6 bg-card border shadow-xs rounded-lg">
             <div className="flex items-center gap-4">
-                <Zap size={isMobile ? 32 : 48} className="stroke-primary-foreground" />
-                <h1 className="text-primary-foreground text-3xl xl:text-4xl font-black">Did you know?</h1>
+                <Zap size={isMobile ? 32 : 48} className="stroke-card-foreground" />
+                <h1 className="text-card-foreground text-3xl xl:text-4xl font-black">Did you know?</h1>
             </div>
-            <p className="mt-4 text-primary-foreground/80 text-lg">A whopping <span className="font-black">73%</span> of all stops lead to an arrest</p>
+            <p className="mt-4 text-card-foreground/80 text-lg">A whopping <span className="font-black">73%</span> of all stops lead to an arrest</p>
         </div>
     )
 }
