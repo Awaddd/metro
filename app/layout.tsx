1331bfe5ae426a088afd2bd5559d103eeed607ad
Awaddd <34748292+Awaddd@users.noreply.github.com>
2025-06-22 17:16:03 +0100

    WIP: prepare to add date filter

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,14 +34,25 @@ export default function RootLayout({
 function Header() {
     return (
         <SidebarInset>
-            <header className="flex h-[70px] shrink-0 items-end gap-2 bg-sidebar">
-                <div className="flex items-center gap-2 px-4 pb-0.5">
+            <header className="flex h-[70px] shrink-0 items-end justify-between gap-2 pb-0.5 bg-sidebar">
+                <div className="flex items-center gap-2 px-4">
                     <SidebarTrigger className="-ml-1 cursor-pointer" />
                     <Separator
                         orientation="vertical"
-                        className="mr-2 data-[orientation=vertical]:h-4"
+                        className="data-[orientation=vertical]:h-4 bg-gray-900/50"
                     />
-                    <h2>Overview</h2>
+                    <h2 className="px-2">Overview</h2>
+                    <Separator
+                        orientation="vertical"
+                        className="data-[orientation=vertical]:h-4 bg-gray-900/50"
+                    />
+                    <div className="px-2">
+                        <span>Showing results for <span className="underline">Aug 2024</span></span>
+                        <span className="py-1 px-2.5 ml-3 bg-primary/20 text-primary rounded-lg text-sm font-black">Latest</span>
+                    </div>
+                </div>
+                <div className="pr-8">
+                    {/* month picker will go here */}
                 </div>
             </header>
         </SidebarInset>
--- components/InfoCard.tsx
+++ components/InfoCard.tsx
@@ -16,7 +16,7 @@ export default function ({ }: Props) {
                 <Zap size={isMobile ? 32 : 48} className="stroke-primary fill-yellow-300" />
                 <h1 className="text-card-foreground text-3xl xl:text-4xl font-black">Did you know?</h1>
             </div>
-            <p className="mt-4 text-card-foreground/80 text-lg">A whopping <span className="font-black">73%</span> of all stops lead to an arrest</p>
+            <p className="mt-4 text-card-foreground/80 text-lg">A whopping <strong>73%</strong> of all stops lead to an arrest</p>
         </div>
     )
 }
