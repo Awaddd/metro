7a2b15615ff0769b933d52d1759445823a1606ff
Awaddd <34748292+Awaddd@users.noreply.github.com>
2025-06-23 01:17:14 +0100

    WIP: batch requests

--- app/actions/get-stop-and-search-data.ts
+++ app/actions/get-stop-and-search-data.ts
@@ -1,8 +1,31 @@
-import data from "@/testData/metro-aug-24.json";
-import { StopSearchResponse } from "@/types/stop-search";
+import getAvailableDates from "./available-dates";
 
-export default async function getStopSearchData() {
-  // working with test data for aug 24, the most recent date that has data
+export default async function getStopSearchData(dateString: string) {
+  let date;
 
-  (data as StopSearchResponse[]).forEach((item) => {});
+  if (dateString) {
+    date = new Date(dateString);
+  }
+
+  const availableDates = await getAvailableDates();
+  batch(availableDates, 10);
+}
+
+function fetchData(date: string) {
+  return new Promise<void>((resolve) => {
+    setTimeout(() => {
+      console.log("fetched", date);
+      resolve();
+    }, 1000);
+  });
+}
+
+async function batch(dates: string[], size: number) {
+  console.log("batching array in chunks of", size);
+  for (let i = 0; i < dates.length; i += size) {
+    console.log("executing batch ", i);
+    const chunk = dates.slice(i, i + size);
+    const promises = chunk.map((date) => fetchData(date));
+    await Promise.all(promises);
+  }
 }
