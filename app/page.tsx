d155ea2c638d1a51f2f7e2420bedcf82c2248170
Awaddd <34748292+Awaddd@users.noreply.github.com>
2025-06-26 14:52:30 +0100

    Fix error

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default async function Home() {
 
     await queryClient.prefetchQuery({
         queryKey: getQueryKey(),
-        queryFn: () => getStopSearchData()
+        queryFn: () => getStopSearchData({})
     })
 
     return (
